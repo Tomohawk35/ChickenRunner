8c984b023ca6a82724fcc5f7357a8530ddef48e5
Tyler <83353939+Tomohawk35@users.noreply.github.com>
2026-06-25 23:42:33 -0400

    victory scene is working

--- chicken-runner/chicken/chicken.gd
+++ chicken-runner/chicken/chicken.gd
@@ -7,6 +7,7 @@ var speed : float = 200.0
 
 func _ready() -> void:
 	area_2d.body_entered.connect(_on_body_entered)
+	area_2d.area_entered.connect(_on_area_entered)
 
 func _physics_process(_delta: float) -> void:
 	var dir : Vector2 = _get_direction()
@@ -15,10 +16,12 @@ func _physics_process(_delta: float) -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is MovingObject:
-		print("Hit by car")
 		GameManager.change_scene("game_over")
-	else:
-		print("Some other body entered...")
+
+func _on_area_entered(area: Area2D) -> void:
+	if area is CompletionZone:
+		print("Completion Zone reached!")
+		GameManager.change_scene("victory")
 
 func _get_direction() -> Vector2:
 	return Input.get_vector("left", "right", "up", "down")
