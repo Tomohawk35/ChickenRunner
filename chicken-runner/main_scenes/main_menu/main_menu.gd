bf54a6f7106db17092d7f4abcfe5a6b92daf5b9f
Tyler <83353939+Tomohawk35@users.noreply.github.com>
2026-06-29 23:11:41 -0400

    Added credits to main menu

--- chicken-runner/main_scenes/main_menu/main_menu.gd
+++ chicken-runner/main_scenes/main_menu/main_menu.gd
@@ -6,11 +6,16 @@ class_name MainMenu
 @onready var exit_button: Button = %ExitButton
 @onready var credits_button: Button = %CreditsButton
 
+@onready var credits_panel: PanelContainer = %CreditsPanel
+@onready var close_button: Button = %CloseButton
+
 func _ready() -> void:
 	start_game_button.pressed.connect(_on_start_game_button_pressed)
 	options_button.pressed.connect(_on_options_button_pressed)
 	credits_button.pressed.connect(_on_credits_button_pressed)
 	exit_button.pressed.connect(_on_exit_button_pressed)
+	close_button.pressed.connect(_on_close_button_pressed)
+	credits_panel.hide()
 
 func _on_start_game_button_pressed() -> void:
 	GameManager.new_game()
@@ -19,7 +24,10 @@ func _on_options_button_pressed() -> void:
 	pass
 
 func _on_credits_button_pressed() -> void:
-	pass # TODO: 
+	credits_panel.show()
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+func _on_close_button_pressed() -> void:
+	credits_panel.hide()
