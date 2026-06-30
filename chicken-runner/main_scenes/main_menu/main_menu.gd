dd42db74b381230f319b78a1df84449e51b93058
Tyler <83353939+Tomohawk35@users.noreply.github.com>
2026-06-29 20:53:39 -0400

    small tweaks

--- chicken-runner/main_scenes/main_menu/main_menu.gd
+++ chicken-runner/main_scenes/main_menu/main_menu.gd
@@ -4,10 +4,12 @@ class_name MainMenu
 @onready var start_game_button: Button = %StartGameButton
 @onready var options_button: Button = %OptionsButton
 @onready var exit_button: Button = %ExitButton
+@onready var credits_button: Button = %CreditsButton
 
 func _ready() -> void:
 	start_game_button.pressed.connect(_on_start_game_button_pressed)
 	options_button.pressed.connect(_on_options_button_pressed)
+	credits_button.pressed.connect(_on_credits_button_pressed)
 	exit_button.pressed.connect(_on_exit_button_pressed)
 
 func _on_start_game_button_pressed() -> void:
@@ -15,6 +17,9 @@ func _on_start_game_button_pressed() -> void:
 
 func _on_options_button_pressed() -> void:
 	pass
+
+func _on_credits_button_pressed() -> void:
+	pass # TODO: 
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
