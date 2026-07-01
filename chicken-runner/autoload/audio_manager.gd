9688ee8d4c0d2bdb927451e8c00c56dc766c258c
Minicomp-pixel <jaredjael523@gmail.com>
2026-07-01 18:43:37 -0400

    added some sfx

--- chicken-runner/autoload/audio_manager.gd
+++ chicken-runner/autoload/audio_manager.gd
@@ -5,6 +5,7 @@ extends Node
 const CHICKEN_SFX_UI_BEEPS_ACTIVATE : AudioStreamWAV = preload("uid://smmmbuk7becd")
 const CHICKEN_SFX_UI_BEEPS : AudioStreamWAV = preload("uid://mb6elb3vlre7")
 const CHICKEN_SFX_UI_BEEP_1 : AudioStreamWAV = preload("uid://djix8s06erfaa")
+const CHICKEN_SFX_GETTING_HIT_IMPACT : AudioStreamWAV = preload("uid://dtslptl8ip8s1")
 
 ## MUSIC
 const PROJECT_76_3 : AudioStreamMP3 = preload("uid://be5eusl3scf52") # TODO: Replace with WAV file
@@ -14,7 +15,6 @@ const PROJECT_76_3 : AudioStreamMP3 = preload("uid://be5eusl3scf52") # TODO: Rep
 
 func _ready() -> void:
 	play_background_music()
-	play_button_click_sound()
 
 ## Button Hover Sound
 func play_button_hover_sound() -> void:
@@ -30,3 +30,7 @@ func play_button_click_sound() -> void:
 func play_background_music() -> void:
 	background_music_player.stream = PROJECT_76_3
 	background_music_player.play()
+
+func play_chicken_hit_sound() -> void:
+	sound_effects_player.stream = CHICKEN_SFX_GETTING_HIT_IMPACT
+	sound_effects_player.play()
