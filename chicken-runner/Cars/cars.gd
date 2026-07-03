5600feb7ca2da3b75e407401ff8b9dae040c2ddf
Tyler <83353939+Tomohawk35@users.noreply.github.com>
2026-07-03 12:07:15 -0400

    Working on implementing a base moving object class

--- chicken-runner/Cars/cars.gd
+++ chicken-runner/Cars/cars.gd
@@ -1,36 +1,36 @@
-extends CharacterBody2D
-class_name MovingObject
+extends MovingObject
+class_name Car
 
-@export var speed : float = 300.0
+#@export var speed : float = 300.0
+#
+#var direction : Vector2 = Vector2.RIGHT
+#var has_entered_screen : bool = false
+#
+#@onready var sprite: Sprite2D = $Sprite2D
 
-var direction : Vector2 = Vector2.RIGHT
-var has_entered_screen : bool = false
+#func _ready() -> void:
+	#$Area2D.body_entered.connect(_on_area_2d_body_entered)
 
-@onready var sprite: Sprite2D = $Sprite2D
-
-func _ready() -> void:
-	$Area2D.body_entered.connect(_on_area_2d_body_entered)
-
-func _process(_delta: float) -> void:
-	if global_position.x > 4000 or global_position.x < -200:
-		queue_free()
-	pass
-
-func _physics_process(_delta: float) -> void:
-	velocity = direction.normalized() * speed
-	
-	move_and_slide()
-
-
-func set_direction(new_direction: Vector2):
-	direction = new_direction
-	
-	if sprite:
-		sprite.flip_h = (direction == Vector2.LEFT)
-
-
-func set_speed(new_speed: float):
-	speed = new_speed
+#func _process(_delta: float) -> void:
+	#if global_position.x > 4000 or global_position.x < -200:
+		#queue_free()
+	#pass
+#
+#func _physics_process(_delta: float) -> void:
+	#velocity = direction.normalized() * speed
+	#
+	#move_and_slide()
+#
+#
+#func set_direction(new_direction: Vector2):
+	#direction = new_direction
+	#
+	#if sprite:
+		#sprite.flip_h = (direction == Vector2.LEFT)
+#
+#
+#func set_speed(new_speed: float):
+	#speed = new_speed
 
 
 #func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
@@ -42,7 +42,7 @@ func set_speed(new_speed: float):
 		#queue_free()
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is Chicken:
-		print("I hit the chicken!")
-		GameManager.Hit.emit()
+#func _on_area_2d_body_entered(body: Node2D) -> void:
+	#if body is Chicken:
+		#print("I hit the chicken!")
+		#GameManager.Hit.emit()
