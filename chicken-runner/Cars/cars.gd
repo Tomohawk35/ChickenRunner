cb8aebfaf4381c1797cd6a22e55f295ee7faa354
Tyler <83353939+Tomohawk35@users.noreply.github.com>
2026-07-04 13:17:25 -0400

    Commented out code as most of it is covered in the inherited class

--- chicken-runner/Cars/cars.gd
+++ chicken-runner/Cars/cars.gd
@@ -1,24 +1,24 @@
 extends MovingObject
 class_name Car
 
-@export var speed : float = 300.0
-
-var direction : Vector2 = Vector2.RIGHT
-
-@onready var sprite: Sprite2D = $Sprite2D
-
-func _process(_delta: float) -> void:
-	if global_position.x > 2000 or global_position.x < -200:
-		queue_free()
-
-func _physics_process(_delta: float) -> void:
-	velocity = direction.normalized() * speed
-	move_and_slide()
-
-func set_direction(new_direction: Vector2):
-	direction = new_direction
-	if sprite:
-		sprite.flip_h = (direction == Vector2.LEFT)
-
-func set_speed(new_speed: float):
-	speed = new_speed
+#@export var speed : float = 300.0
+#
+#var direction : Vector2 = Vector2.RIGHT
+#
+#@onready var sprite: Sprite2D = $Sprite2D
+#
+#func _process(_delta: float) -> void:
+	#if global_position.x > 2000 or global_position.x < -200:
+		#queue_free()
+#
+#func _physics_process(_delta: float) -> void:
+	#velocity = direction.normalized() * speed
+	#move_and_slide()
+#
+#func set_direction(new_direction: Vector2):
+	#direction = new_direction
+	#if sprite:
+		#sprite.flip_h = (direction == Vector2.LEFT)
+#
+#func set_speed(new_speed: float):
+	#speed = new_speed
