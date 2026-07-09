a58dfe7a8985a995588e91a2a729ad12e2b508f4
Tyler <83353939+Tomohawk35@users.noreply.github.com>
2026-07-09 19:01:19 -0400

    Added variables to handle cooldown and oneshot effects

--- chicken-runner/items/power_ups/power_up.gd
+++ chicken-runner/items/power_ups/power_up.gd
@@ -5,8 +5,10 @@ class_name Equipment
 @export var description : String = "Description of the New Equipment"
 @export var icon : Texture2D
 
-@export var equip_effects : Array[ItemEffect] = []
-@export var use_effects : Array[ItemEffect] = []
+@export var equip_effects : Array[ItemEquipEffect] = []
+@export var use_effects : Array[ItemUseEffect] = []
+@export var cooldown : float = 10.0
+@export var one_shot : bool = false
 
 func equip() -> void:
 	for effect in equip_effects:
