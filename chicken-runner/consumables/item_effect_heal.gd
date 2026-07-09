b89fc39b147768d0f6bc81f7a5a751129d167248
Tyler <83353939+Tomohawk35@users.noreply.github.com>
2026-07-08 23:07:20 -0400

    Update item effect heal to have configurable heal amount

--- chicken-runner/consumables/item_effect_heal.gd
+++ chicken-runner/consumables/item_effect_heal.gd
@@ -1,5 +1,7 @@
 extends ItemEffect
 class_name ItemEffectHeal
 
+@export var heal_amount: float = 20.0
+
 func activate() -> void:
-	PlayerManager.increase_heat(-30)
+	PlayerManager.increase_heat(-heal_amount)
