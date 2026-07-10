27b04da419371f26ed574a394a6fd8c94f50ebae
Tyler <83353939+Tomohawk35@users.noreply.github.com>
2026-07-09 23:27:20 -0400

    Created projectile class

---
+++ chicken-runner/projectile/projectile.gd
@@ -0,0 +1,31 @@
+extends Area2D
+class_name Projectile
+
+
+var _velocity : Vector2
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _physics_process(delta: float) -> void:
+	translate(_velocity * delta)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("moving_objects"):
+		body.queue_free() # FIXME : Move to moving object script to incorporate a destroy effect
+		queue_free()
+
+func set_velocity(speed: float, dir: Vector2) -> void:
+	_velocity = speed * dir
+	_set_projectile_direction(dir)
+
+func _set_projectile_direction(dir: Vector2) -> void:
+	match dir:
+		Vector2.UP:
+			pass
+		Vector2.RIGHT:
+			rotation_degrees += 90.0
+		Vector2.LEFT:
+			rotation_degrees -= 90.0
+		Vector2.DOWN:
+			rotation_degrees += 180.0
