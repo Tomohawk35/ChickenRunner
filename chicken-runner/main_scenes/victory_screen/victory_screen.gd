3acc65bd644ccb961b6e4c5421d5c47a08afdda8
MagicalCoder <kuppamajith@gmail.com>
2026-07-11 20:58:54 +0530

    Added sound to victory scene
    I forgot to add the sound to victory scene earlier so now i added it.

--- chicken-runner/main_scenes/victory_screen/victory_screen.gd
+++ chicken-runner/main_scenes/victory_screen/victory_screen.gd
@@ -7,9 +7,16 @@ class_name VictoryScreen
 func _ready() -> void:
 	menu_button.pressed.connect(_on_menu_button_pressed)
 	exit_button.pressed.connect(_on_exit_button_pressed)
-
+	menu_button.mouse_entered.connect(_on_button_hover)
+	exit_button.mouse_entered.connect(_on_button_hover)
+	
 func _on_menu_button_pressed() -> void:
+	AudioManager.play_button_click_sound()
 	GameManager.change_scene("main_menu")
 
 func _on_exit_button_pressed() -> void:
+	AudioManager.play_button_click_sound()
 	get_tree().quit()
+
+func _on_button_hover() -> void:
+	AudioManager.play_button_hover_sound()
