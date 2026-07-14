6d26b51f566f96852c81cecb725fcc35c057254c
Tyler <tyler13ho@gmail.com>
2026-07-14 09:18:36 -0400

    Incorporated move speed modifier for biker goggles

--- chicken-runner/autoload/player_manager.gd
+++ chicken-runner/autoload/player_manager.gd
@@ -3,6 +3,7 @@ extends Node
 
 signal heat_changed(new_heat_level: float)
 signal heat_gain_rate_changed(new_heat_gain_rate: float)
+signal move_speed_changed(new_move_speed: float)
 signal consumable_changed
 
 var consumable : Consumable = load("uid://c3nv13iw6iay3")
@@ -19,6 +20,11 @@ var heat_level : float = 0.0 :
 	set(v):
 		heat_level = v
 		heat_changed.emit(heat_level)
+
+var move_speed : float = 0.0 : 
+	set(v):
+		move_speed = v
+		move_speed_changed.emit(move_speed)
 
 var _cooling_count : int = 0
 var is_cooling : bool:
