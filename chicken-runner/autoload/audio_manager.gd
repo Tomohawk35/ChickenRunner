8c6aebb482fa32c8c8b3cb8d9da27caad51b3008
Minicomp-pixel <jaredjael523@gmail.com>
2026-07-16 21:05:13 -0400

    added sfx to code

--- chicken-runner/autoload/audio_manager.gd
+++ chicken-runner/autoload/audio_manager.gd
@@ -5,6 +5,18 @@ extends Node
 const CHICKEN_SFX_UI_BEEPS_ACTIVATE : AudioStreamWAV = preload("uid://smmmbuk7becd")
 const CHICKEN_SFX_UI_BEEP_1 : AudioStreamWAV = preload("uid://djix8s06erfaa")
 const CHICKEN_SFX_GETTING_HIT_IMPACT : AudioStreamWAV = preload("uid://dtslptl8ip8s1")
+const CHICKEN_SFX_1_COCKLEDOODLEDOO : AudioStreamMP3 = preload("res://assets/sfx/CHICKEN SFX 1 COCKLEDOODLEDOO.mp3")
+const CAR_DRIVING_BY_WITH_HONK : AudioStreamMP3 = preload("res://assets/sfx/CAR DRIVING BY WITH HONK.mp3")
+const CHICKEN_EGG_SHOOT : AudioStreamMP3 = preload("res://assets/sfx/CHICKEN EGG SHOOT.mp3")
+const CHICKEN_GOLD_FEATHER : AudioStreamMP3 = preload("res://assets/sfx/CHICKEN GOLD FEATHER.mp3")
+const CHICKEN_SFX_BA_CAW___COPY : AudioStreamMP3 = preload("res://assets/sfx/CHICKEN SFX BA CAW - Copy.mp3")
+const CHICKEN_SFX_POWER_UP : AudioStreamMP3 = preload("res://assets/sfx/CHICKEN SFX POWER UP.mp3")
+const CHICKEN_STEP_FORWARD_MIXED_SINGLE : AudioStreamMP3 = preload("res://assets/sfx/CHICKEN Step Forward MIXED SINGLE.mp3")
+const CHICKEN_WALKING_ON_PUDDLE : AudioStreamMP3 = preload("res://assets/sfx/CHICKEN Walking on Puddle.mp3")
+const DIE_OF_HEAT : AudioStreamMP3 = preload("res://assets/sfx/DIE OF HEAT.mp3")
+const HOT_SIZZLING___SOUND_EFFECT : AudioStreamMP3 = preload("res://assets/sfx/Hot Sizzling - Sound Effect.mp3")
+const SNAKE_HISSING : AudioStreamMP3 = preload("res://assets/sfx/SNAKE HISSING.mp3")
+const WATER_COOL_OFF : AudioStreamMP3 = preload("res://assets/sfx/WATER COOL OFF.mp3")
 
 
 ## MUSIC
@@ -33,4 +45,8 @@ func play_background_music() -> void:
 
 func play_chicken_hit_sound() -> void:
 	sound_effects_player.stream = CHICKEN_SFX_GETTING_HIT_IMPACT
+	sound_effects_player.play()
+
+func play_chicken_cockledoodledoo_victory_sound() -> void:
+	sound_effects_player.stream = CHICKEN_SFX_1_COCKLEDOODLEDOO
 	sound_effects_player.play()
